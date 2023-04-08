591e779e5fd68a2ecf3a8aec7d9253bd8faa1a42
Kushagra Gupta <kushgpt08@gmail.com>
2023-04-08 09:21:00 +0000

    Scaffolding for parsing messages

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 The [fly.io distributed systems challenges](https://fly.io/dist-sys/) solved in Rust.
 
 Followed along with [Solving distributed systems challenges in Rust](https://youtu.be/gboGyccRVXI) by [Jon Gjengset](https://github.com/jonhoo)
+
+## Maelstrom
+
+This project was tested with [v0.2.3](https://github.com/jepsen-io/maelstrom/releases/tag/v0.2.3) of [Maelstrom](https://github.com/jepsen-io/maelstrom).
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,44 @@
-fn main() {
-    println!("Hello, world!");
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Serialize, Deserialize, Clone, PartialEq, Eq)]
+struct Message {
+    src: String,
+    #[serde(rename = "dest")]
+    dst: String,
+    body: Body,
+}
+
+type MsgId = i64;
+
+#[derive(Debug, Serialize, Deserialize, Clone, PartialEq, Eq)]
+struct Body {
+    #[serde(rename = "msg_id")]
+    id: Option<MsgId>,
+    in_reply_to: Option<MsgId>,
+    #[serde(flatten)]
+    payload: Payload,
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone, PartialEq, Eq)]
+#[serde(tag = "type")]
+#[serde(rename_all = "snake_case")]
+enum Payload {
+    Echo { echo: String },
+}
+
+const MSG: &str = r#"
+{
+    "src":"123",
+    "dest":"456",
+    "body": {
+        "type": "echo",
+        "msg_id": 1,
+        "echo": "echo 123"
+    }
+}"#;
+
+fn main() -> anyhow::Result<()> {
+    let payload: Message = serde_json::from_slice(MSG.as_bytes())?;
+    println!("{payload:?}");
+    Ok(())
 }
